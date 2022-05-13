d351ae0530ebe0f6ec39e38d9066e300e29f007d
AresOsredkar <aresosredkar1337@gmail.com>
2022-05-13 09:04:06 +0200

    updates

--- Feed.php
+++ Feed.php
@@ -1,6 +1,15 @@
 <?php include 'preset.php'?>
             <?php 
-            
+                $username = $_POST["user"];
+                $text = $_POST["content"];
+                $ann = ""
+                if(isset($_POST["announce"])){
+                    $ann = " ann";
+                }
+                $link = $_POST["link"];
+                $postDiv = "<div class='post" . $ann . "'><div class='thumbnail'><img src='" . $link . "'></div><div class='username'>" . $username 
+                . "<br>" . date() . "</div><div class='postContent'>" . $text . "</div></div>";
+                echo $postDiv;
             ?>
        
     
--- Post.php
+++ Post.php
@@ -5,13 +5,26 @@
                 color:white;
             }
         </style>
-            <form action="Feed.php" method="post">
+        <br><br>
+            <form action="Post.php" method="post">
                 Username: <input type="text" name="user"><br><br>
-                Text: <textarea name="content" rows="4" cols="50"><br><br>
+                Text: <textarea name="content" rows="4" cols="50"></textarea><br><br>
                 Announcement: <input type="checkbox" name="announce"><br><br>
                 Thumbnail: <input type="url" name="link"><br><br>
                 <input type="submit" value="Post">
             </form>
         </div>
+        <?php 
+                $username = $_POST["user"];
+                $text = $_POST["content"];
+                $ann = "";
+                if(isset($_POST["announce"])){
+                    $ann = " ann";
+                }
+                $link = $_POST["link"];
+                $postDiv = "<div class='post" . $ann . "'><div class='thumbnail'><img src='" . $link . "'></div><div class='username'>" . $username 
+                . "<br>" . date("l jS \of F Y h:i:s A") . "</div><div class='postContent'>" . $text . "</div></div>";
+                echo $postDiv;
+            ?>
     </body>
 </html>
