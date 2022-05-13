b7248333a1c550770d4a0d08ef315a8af61bae4d
AresOsredkar <aresosredkar1337@gmail.com>
2022-05-13 10:27:17 +0200

    file upload

--- Account.php
+++ Account.php
@@ -1,7 +1,16 @@
 <?php include 'preset.php'?>
             <h1>Account details</h1>
             <p>Here you will be able to change all the account settings, with exclusevly php when we get there.</p>
-            <img src="img/placeholderavatar.png" width="200">
+            <?php
+            if(isset($_POST["img"]))
+            {
+                echo "<img src='" . "img/slika.jpg" . "' width='200'>";
+            }
+            else{
+                echo "<img src='img/placeholderavatar.png' width='200'>";
+            }
+            
+            ?>
             <p>Placeholder image representing an avatar picture</p>
             <pre>
                 Name:
--- Post.php
+++ Post.php
@@ -1,30 +1,63 @@
 <?php include 'preset.php'?>
-        <style>
-            #Browse{
-                background-color:blueviolet;
-                color:white;
+<?php
+        echo "<style>";
+        echo "    #Browse{";
+        echo "background-color:blueviolet;";
+        echo "color:white;";
+        echo "    }";
+        echo "</style>";
+        echo "<br><br>";
+
+        if(isset($_POST["user"])){
+            $username = $_POST["user"];
+            $text = $_POST["content"];
+            $ann = "";
+            $chk = "";
+            if(isset($_POST["announce"])){
+                $ann = " ann";
+                $chk = "checked";
             }
-        </style>
-        <br><br>
-            <form action="Post.php" method="post">
-                Username: <input type="text" name="user"><br><br>
-                Text: <textarea name="content" rows="4" cols="50"></textarea><br><br>
-                Announcement: <input type="checkbox" name="announce"><br><br>
-                Thumbnail: <input type="url" name="link"><br><br>
-                <input type="submit" value="Post">
-            </form>
-        </div>
-        <?php 
-                $username = $_POST["user"];
-                $text = $_POST["content"];
-                $ann = "";
-                if(isset($_POST["announce"])){
-                    $ann = " ann";
-                }
-                $link = $_POST["link"];
-                $postDiv = "<div class='post" . $ann . "'><div class='thumbnail'><img src='" . $link . "'></div><div class='username'>" . $username 
-                . "<br>" . date("l jS \of F Y h:i:s A") . "</div><div class='postContent'>" . $text . "</div></div>";
-                echo $postDiv;
-            ?>
-    </body>
-</html>
+            $link = $_POST["link"];
+            $postDiv = "<div class='post" . $ann . "'><div class='thumbnail'><img src='" . $link . "'></div><div class='username'>" . $username 
+            . "<br>" . date("l jS \of F Y h:i:s A") . "</div><div class='postContent'>" . $text . "</div></div>";
+            echo $postDiv;
+        }
+        $okej = false;
+        $napaka = "";
+        if(isset($_GET["ack"])){
+            $okej = true;
+
+            if(strlen($username)<3 || strlen($username)>15){
+                $okej = false;
+                echo "<b>Ime ima lahko od 3 do 15 znakov</b>";
+            }
+            if(strlen($text)>255){
+                echo "<b>Besedilo ima lahko do 255 znakov</b>"
+                $okej = false;
+            }
+            
+        }
+        if(!$okej)
+        {
+            echo "    <form action='Post.php?akc=1' method='post'>";
+            echo "  Username: <input type='text' name='user'><br><br>";
+            echo "  Text: <textarea name='content' rows='4' cols='50'></textarea><br><br>";
+            echo "  Announcement: <input type='checkbox' name='announce'><br><br>";
+            echo "  Thumbnail: <input type='url' name='link'><br><br>";
+            echo "  <input type='submit' value='Post'>";
+            echo "</form>";
+        }
+        else{
+            echo "    <form action='Post.php?akc=1' method='post'>";
+            echo "  Username: <input type='text' name='user' value='". $username . "'><br><br>";
+            echo "  Text: <textarea name='content' rows='4' cols='50' value='" . $text . "'></textarea><br><br>";
+            echo "  Announcement: <input type='checkbox' name='announce' " . $chk . "><br><br>";
+            echo "  Thumbnail: <input type='url' name='link' value='" . $link . "'><br><br>";
+            echo "  <input type='submit' value='Post'><br><br>";
+            echo "</form>";
+        }
+
+        echo "</div>";       
+        echo "</body>";
+        echo "</html>";
+?>
